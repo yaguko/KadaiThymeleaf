8f52cdb2016cd551ad598ee892afff57acb9dd7a
yaguko <yaguko_7@yahoo.co.jp>
2023-12-10 19:30:25 +0900

    15章課題

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -1,10 +1,8 @@
 package com.techacademy;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.stereotype.Controller;
 
 public class OutputController {
 
